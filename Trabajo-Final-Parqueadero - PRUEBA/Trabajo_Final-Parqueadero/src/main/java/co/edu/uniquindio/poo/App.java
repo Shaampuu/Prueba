21bd497a99f158d893700d6a8147c0f989eedb10
Jurden <daniele.juradoc@uqvirtual.edu.co>
2024-05-20 18:20:47 -0500

    error abundacia de ;

--- Trabajo-Final-Parqueadero - PRUEBA/Trabajo_Final-Parqueadero/src/main/java/co/edu/uniquindio/poo/App.java
+++ Trabajo-Final-Parqueadero - PRUEBA/Trabajo_Final-Parqueadero/src/main/java/co/edu/uniquindio/poo/App.java
@@ -4,6 +4,6 @@ public class App {
     public static void main(String[] args) {
         Parqueadero parqueadero = new Parqueadero(3, 3);
         AppController appController = new AppController(parqueadero);
-        appController.iniciar();;
+        appController.iniciar();
     }
 }
