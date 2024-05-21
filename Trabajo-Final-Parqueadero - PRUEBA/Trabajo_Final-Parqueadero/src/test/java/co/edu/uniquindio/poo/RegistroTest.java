c62277b0c9534b12d67e31c4c443cf708a2f1553
Jurden <daniele.juradoc@uqvirtual.edu.co>
2024-05-20 19:24:51 -0500

    espacios

--- Trabajo-Final-Parqueadero - PRUEBA/Trabajo_Final-Parqueadero/src/test/java/co/edu/uniquindio/poo/RegistroTest.java
+++ Trabajo-Final-Parqueadero - PRUEBA/Trabajo_Final-Parqueadero/src/test/java/co/edu/uniquindio/poo/RegistroTest.java
@@ -9,12 +9,13 @@ public class RegistroTest {
     private Registro registro;
 
     @BeforeEach
-public void setUp() {
-    Vehiculo vehiculo = new Carro("ABC123", 2022, new Propietario("Juan"), TipoCarro.CAMIONETA);
-    LocalDateTime fechaEntrada = LocalDateTime.of(2024, 5, 20, 8, 0); // Ejemplo de fecha de entrada
-    LocalDateTime fechaSalida = LocalDateTime.of(2024, 5, 20, 12, 0); // Ejemplo de fecha de salida
-    registro = new Registro(vehiculo, fechaEntrada, fechaSalida);
-}
+    public void setUp() {
+        Vehiculo vehiculo = new Carro("ABC123", 2022, new Propietario("Juan"), TipoCarro.CAMIONETA);
+        LocalDateTime fechaEntrada = LocalDateTime.of(2024, 5, 20, 8, 0); // Ejemplo de fecha de entrada
+        LocalDateTime fechaSalida = LocalDateTime.of(2024, 5, 20, 12, 0); // Ejemplo de fecha de salida
+        registro = new Registro(vehiculo, fechaEntrada, fechaSalida);
+    }
+
     @Test
     public void testCalcularCosto() {
         // Se espera que el costo sea 4 * tarifaPorHora
